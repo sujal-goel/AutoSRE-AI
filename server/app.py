99a45f5b8149081ef6a2e8b144d108c9dd0379fb
sujal goel <sujalgoel19@gmail.com>
2026-04-08 14:05:13 +0530

    fix: UV lock and server run file added in the pyptoject.toml

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from app.api.routes import app
 import uvicorn
-
-if __name__ == '__main__':
-
+def main():
     uvicorn.run("app.api.routes:app", host="0.0.0.0", port=7860, reload=False)
+if __name__ == "__main__":
+    main()
